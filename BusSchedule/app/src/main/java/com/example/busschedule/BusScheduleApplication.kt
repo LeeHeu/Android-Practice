9fe2e887f15e469d538c218268b8e36ce954be8b
Hiếu Lê <125482658+LeeHeu@users.noreply.github.com>
2024-10-17 14:36:08 +0700

    Delete BusSchedule directory

--- BusSchedule/app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.busschedule
-
-import android.app.Application
-import com.example.busschedule.data.AppDatabase
-
-class BusScheduleApplication: Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
